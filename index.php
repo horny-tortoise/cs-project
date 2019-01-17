7aca20ccf3f4b0cf53966d16e402453aba4808ba
horny-tortoise <46795581+horny-tortoise@users.noreply.github.com>
2019-01-17 20:53:47 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+<?php
+
+$variable = "Jake Loosley";
+
+echo "Hello there, my name is ". $variable;
+?>
+
+</body>
+</html>
